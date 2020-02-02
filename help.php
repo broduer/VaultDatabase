99ee5af1680563196f6d99e95e2f3d9037cb2c31
Tadhg <tadhgsmboyle@gmail.com>
2020-02-01 22:45:37 -0800

    whoops forgot this xD

--- help.php
+++ help.php
@@ -47,7 +47,7 @@
          </div>
          <div class="col-md-3">
             <h4>What data do you collect?</h4>
-            <p>Each time you login and out of VaultMC we save some of your data. This includes (and is not limited to) your Minecraft UUID, Username, IP, In-Game Rank, Firstseen, Lastseen and your Playtime. We use this data to assist with your in-game experience, along with ensuring the safety of our server from things such as bot attacks.</p>
+            <p>Each time you login and out of VaultMC we save some of your data. This includes (and is not limited to) your Minecraft UUID, Username, IP, Rank, First Seen, Last Seen and your Playtime. We use this data to assist with your in-game experience, along with ensuring the safety of our server from things such as bot attacks.</p>
          </div>
          <div class="col-md-4">
             <h4>How do I register?</h4>
@@ -55,7 +55,7 @@
          </div>
          <div class="col-md-3">
             <h4>Where do you get my skin image from?</h4>
-            <p>We get all of our player skin and head renders from the <a href="https://crafatar.com/">Crafatar</a> API service.</p>
+            <p>We get all of our player skin and head renders from the <a href="https://crafatar.com/">Crafatar</a> API service. Thanks jomo!</p>
          </div>
          <div class="col-md-1">
          </div>
@@ -73,8 +73,8 @@
             <p>Great question! If you register on this website, you can customize your timezone while viewing player statistics, edit your clan description and manage the players in your clan (if you are the owner). More to come soon!</p>
          </div>
          <div class="col-md-3">
-            <h4>Is the Vault Database open source?</h4>
-            <p></p>
+            <h4>Is this open source?</h4>
+            <p>Yes! Check out <a href="https://github.com/Aberdeener/VaultDatabase" target="_blank">this repo</a>. I try to commit everytime I update or change something. I am totally open to contributions!</p>
          </div>
          <div class="col-md-1">
          </div>
