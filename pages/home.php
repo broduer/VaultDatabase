91d3d71d829cfc57cbbbd56af058a7ba9c550e05
Tadhg <tadhgsmboyle@gmail.com>
2020-02-23 22:21:53 -0800

    temp homepage change

--- pages/home.php
+++ pages/home.php
@@ -1,34 +1,19 @@
-<div class="row">
-    <div class="col-md-12">
-        <h1 class="text-center">VaultMC</h1>
-    </div>
-</div>
-<br>
+<!--
 <div class="row" align="center">
-    <div class="col-md-3">
+    <div class="col-md-2">
     </div>
-    <div class="col-md-6">
-        <div class="row" style="background-color:#303030; border-radius:10px; padding:10px;">
-            <div class="col-md-6">
-                <h3>Player & Clan Information</h3>
-                <form role="form" action="?" method="get">
-                <input type='hidden' name='view' value='search'/>
-                    <div class="form-group">
-                        <label for="playername">Search for a player or clan below</label>
-                        <input type="text" class="form-control" name="query" placeholder="Enter your query here.">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Search</button>
-                </form>
-            </div>
-            <div class="col-md-6">
-                <h3>About this site</h3>
-                <p>This custom interface helps you to view information about players on VaultMC.</p>
-            </div>
+    <div class="col-md-8">
+        <div class="jumbotron" style="background-color:#303030; border-radius:10px; padding:10px;">
+            <h1 class="display-4">VaultMC</h1>
+            <p class="lead">Built with <b><i>Simplicity</i></b> for our <b><i>Community</i></b>.</p>
+            <br>
+            <p></p>
         </div>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-2">
     </div>
 </div>
 <br>
 <br>
+--!>
 <?php include("blog/blog.php") ?>
